a2bc5a804367d9654204fb465a7b96c42c245567
Sheon1380 <44202050+Sheon1380@users.noreply.github.com>
2018-10-16 23:44:44 +0800

    7. Reverse Integer

---
+++ 001-010/Reverse Interger.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while(x!=0){
+            if(abs(res)>INT_MAX/10)
+                return 0;
+            else{
+                res=res*10+x%10;
+                x/=10;
+            }
+        }
+        return res;
+    }
+};
