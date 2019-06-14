6183a9f30a6700771bcc3702c6d42e53c9a59827
YuHsiang Lin <44202050+Sheon1380@users.noreply.github.com>
2019-06-14 16:43:49 +0800

    Update Balanced Binary Tree.cpp

--- 101-110/Balanced Binary Tree.cpp
+++ 101-110/Balanced Binary Tree.cpp
@@ -28,17 +28,18 @@ public:
 
 class solution {
 public:
-int dfsHeight (TreeNode *root) {
-        if (root == NULL) return 0;
+    int dfsHeight (TreeNode *root) {
+            if (root == NULL) return 0;
 
-        int leftHeight = dfsHeight (root -> left);
-        if (leftHeight == -1) return -1;
-        int rightHeight = dfsHeight (root -> right);
-        if (rightHeight == -1) return -1;
+            int leftHeight = dfsHeight (root -> left);
+            if (leftHeight == -1) return -1;
+            int rightHeight = dfsHeight (root -> right);
+            if (rightHeight == -1) return -1;
 
-        if (abs(leftHeight - rightHeight) > 1)  return -1;
-        return max (leftHeight, rightHeight) + 1;
+            if (abs(leftHeight - rightHeight) > 1)  return -1;
+            return max (leftHeight, rightHeight) + 1;
     }
+    
     bool isBalanced(TreeNode *root) {
         return dfsHeight (root) != -1;
     }
