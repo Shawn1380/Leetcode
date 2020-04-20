517b3b17399fdb714f7771b81a5d7206654e8d1b
Yu-Hsiang Lin <44202050+Sheon1380@users.noreply.github.com>
2020-04-20 16:28:18 +0800

    Update Palindrome Number.cpp

--- 001-010/Palindrome Number.cpp
+++ 001-010/Palindrome Number.cpp
@@ -1,14 +1,29 @@
+// C++ solution
 class Solution {
 public:
     bool isPalindrome(int x) {
         int reverse = 0;
-        if(x<0 || (x%10==0 && x!=0))
+        if(x < 0 || (x % 10 == 0 && x != 0))
             return false;
         while(x > reverse){
             reverse = reverse * 10 + x % 10;
-            x/=10;
+            x /= 10;
         }
 
-        return x==reverse || x==reverse/10;
+        return x == reverse || x == reverse/10;
     }
 };
+
+// C solution
+bool isPalindrome(int x){
+    int reverse = 0;
+    if(x < 0 || (x % 10 == 0 && x!=0))
+        return 0;
+    while(x > reverse)
+    {
+        reverse = reverse * 10 + x % 10;
+        x /= 10;
+    }
+    
+    return x == reverse || x == reverse / 10;
+}
